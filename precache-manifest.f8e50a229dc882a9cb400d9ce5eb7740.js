8b843c5156731ce916c5f689fbae86473f8b9dbc
Mikolaj <mikolaj35@wp.pl>
2020-06-09 11:28:55 +0200

    Updates

---
+++ precache-manifest.f8e50a229dc882a9cb400d9ce5eb7740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f44732bbc35f622a030e4b1250982b",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "e507a4e40c2341081783",
+    "url": "/BurgerBuilder/static/css/main.8ddba891.chunk.css"
+  },
+  {
+    "revision": "fa81fa25673f1cef5265",
+    "url": "/BurgerBuilder/static/js/2.3fb7603b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder/static/js/2.3fb7603b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e507a4e40c2341081783",
+    "url": "/BurgerBuilder/static/js/main.bf037848.chunk.js"
+  },
+  {
+    "revision": "cf20594b2cd80f3aad34",
+    "url": "/BurgerBuilder/static/js/runtime-main.74048839.js"
+  }
+]);
