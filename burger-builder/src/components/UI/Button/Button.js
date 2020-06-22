a150ccd615f54f3d3d97fb0fc3b851610830aa2c
Mikolaj <mikolaj35@wp.pl>
2020-06-22 20:37:33 +0200

    Form and validation
    Contact form added,
    Some basic validation if form is filled correctly added,
    Styling for Order button disabled added

--- burger-builder/src/components/UI/Button/Button.js
+++ burger-builder/src/components/UI/Button/Button.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 
 const button = (props) => (
   <button
+    disabled={props.disabled}
     className={[classes.Button, classes[props.btnType]].join(' ')}
     onClick={props.clicked}>{props.children}</button>
 )
@@ -12,7 +13,8 @@ const button = (props) => (
 button.propTypes = {
   btnType: PropTypes.string,
   children: PropTypes.any,
-  clicked: PropTypes.func
+  clicked: PropTypes.func,
+  disabled: PropTypes.bool
 }
 
 export default button
