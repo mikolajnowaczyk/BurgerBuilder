aee1acf9089a1e8bf7d43e9ba62325c23d0380d8
Mikolaj <mikolaj35@wp.pl>
2020-07-01 21:41:23 +0200

    Authentication
    Auth container with basic form validation added

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -5,6 +5,8 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
+
 /* eslint-disable react/prop-types */
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" component={BurgerBuilder} />
         </Switch>
       </Layout>
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -157,7 +157,6 @@ class ContactData extends Component {
     for (let inputIdentifier in updatedOrderForm) {
       formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
     }
-
     this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   }
 
