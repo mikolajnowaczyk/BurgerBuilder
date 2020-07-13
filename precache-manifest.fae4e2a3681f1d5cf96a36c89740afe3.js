b849f2316f986610cee8b939fde0196fbd276497
Mikolaj <mikolaj35@wp.pl>
2020-07-13 13:32:42 +0200

    Updates

---
+++ precache-manifest.fae4e2a3681f1d5cf96a36c89740afe3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849daffc0502b546a77924391fb09893",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "54b4f02f5a05a83f38e0",
+    "url": "/BurgerBuilder/static/css/3.86c8970a.chunk.css"
+  },
+  {
+    "revision": "e7a524173140ce4ce183",
+    "url": "/BurgerBuilder/static/css/4.0684f0c9.chunk.css"
+  },
+  {
+    "revision": "ae7770d65b6b9c10b1fe",
+    "url": "/BurgerBuilder/static/css/5.3153805e.chunk.css"
+  },
+  {
+    "revision": "2b21cf64cd9ec9cb945a",
+    "url": "/BurgerBuilder/static/css/main.db5914f6.chunk.css"
+  },
+  {
+    "revision": "5d9557c1c2315aceff87",
+    "url": "/BurgerBuilder/static/js/2.1a9d5e3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.1a9d5e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b4f02f5a05a83f38e0",
+    "url": "/BurgerBuilder/static/js/3.6d0559da.chunk.js"
+  },
+  {
+    "revision": "e7a524173140ce4ce183",
+    "url": "/BurgerBuilder/static/js/4.79819780.chunk.js"
+  },
+  {
+    "revision": "ae7770d65b6b9c10b1fe",
+    "url": "/BurgerBuilder/static/js/5.64cf3b42.chunk.js"
+  },
+  {
+    "revision": "2b21cf64cd9ec9cb945a",
+    "url": "/BurgerBuilder/static/js/main.7239eb72.chunk.js"
+  },
+  {
+    "revision": "03290a396c2fa37c6791",
+    "url": "/BurgerBuilder/static/js/runtime-main.71a9d8e4.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/logo.b8503d26.png"
+  }
+]);
